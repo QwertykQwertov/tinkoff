c28427c7b1f2a5d87cb41c0cc1b77f260daf6628
QwertykQwertov <qwertykqwertov@mail.ru>
2024-04-02 15:54:32 +0300

    add target _blank to link

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -23,7 +23,7 @@ const handleSubmit = (e) => {
 </script>
 <template>
   <form class="custom-form" @submit.prevent="handleSubmit">
-    <a href="https://artydev.ru/posts/spb-exchange/#%D0%BA%D0%B0%D0%BA-%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C-%D1%82%D0%BE%D0%BA%D0%B5%D0%BD" title="Инструкция для получения токена" ><img class="info-icon" src="../assets/info.svg" alt=""></a>
+    <a href="https://artydev.ru/posts/spb-exchange/#%D0%BA%D0%B0%D0%BA-%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B8%D1%82%D1%8C-%D1%82%D0%BE%D0%BA%D0%B5%D0%BD" title="Инструкция для получения токена" target="_blank"><img class="info-icon" src="../assets/info.svg" alt=""></a>
     <DxTextBox
       :height="height"
       name="token"
